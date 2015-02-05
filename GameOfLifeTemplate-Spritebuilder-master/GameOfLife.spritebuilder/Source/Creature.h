f05379d85b4587df216b7d54f4baa9631acb9980
wentianqi7 <wentianqi7@gmail.com>
2015-02-05 16:17:16 -0500

    code added

---
+++ GameOfLifeTemplate-Spritebuilder-master/GameOfLife.spritebuilder/Source/Creature.h
@@ -0,0 +1,13 @@
+#import "CCSprite.h"
+
+@interface Creature : CCSprite
+
+// stores the current state of the creature
+@property (nonatomic, assign) BOOL isAlive;
+
+// stores the amount of living neighbors
+@property (nonatomic, assign) NSInteger livingNeighbors;
+
+- (id)initCreature;
+
+@end
---
+++ GameOfLifeTemplate-Spritebuilder-master/GameOfLife.spritebuilder/Source/Grid.h
@@ -0,0 +1,20 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Tianqi Wen on 2/2/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+- (void)evolveStep;
+- (void)countNeighbors;
+- (void)updateCreatures;
+
+@end
